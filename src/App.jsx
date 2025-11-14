918127f64553f831cea44dfeb7c28b7230198fb1
giannivitale <giavitale78@gmail.com>
2025-11-14 18:05:04 +0100

    add react-router-dom for routing and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import HomePage from "./???";
 
 
-
-
-function App() {
-  
-
+export default function App(){
   return (
-    <>
-    
-    
-
-
-
-    </>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path='/????' element{<HomePage />} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
