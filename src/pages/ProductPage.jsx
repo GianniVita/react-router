c6830d59ca54ae1e7188a3d1f1cc5aab637c5b6b
giannivitale <giavitale78@gmail.com>
2025-11-17 09:22:56 +0100

    fix: handle errors in product fetching and initialize error state in ProductPage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -4,12 +4,16 @@ import axios from 'axios';
 
 const useProduct = () => {
     const [products, setProducts] = useState([]);
-
+    const [error, setError] = useState(null);
 
 
     useEffect(() =>{
         axios.get('https://fakestoreapi.com/products')
-
+            .then(response => {
+                setProducts(response.data);
+                setError(null);
+            })
+            
 
     })
 
