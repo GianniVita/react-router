0e76c31d1eb8b50d5bfd390e4c089cba75ac447e
giannivitale <giavitale78@gmail.com>
2025-11-17 17:55:12 +0100

    add NotFoundPage component for handling 404 errors

---
+++ src/pages/404Page.jsx
@@ -0,0 +1,12 @@
+export default function NotFoundPage(){
+    return (
+        <div className="text-center mt-5">
+            <h1>404 - Pagina non Trovata</h1>
+            <p>La pagina che stai cercando non esiste.</p>
+            <button className="btn btn-primary" onClick={() => }Torna alla Home></button>
+
+        </div>
+    )
+
+
+}
